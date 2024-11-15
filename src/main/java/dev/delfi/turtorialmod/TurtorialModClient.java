26b30023d81d41bd3742e6e3605279775ca8b767
Delfi-CH <laspitzer@gmx.ch>
2024-11-15 10:55:00 +0100

    Initial commit

---
+++ src/main/java/dev/delfi/turtorialmod/TurtorialModClient.java
@@ -0,0 +1,10 @@
+package dev.delfi.turtorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TurtorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
